4d96700ab686236c6b0ae107cb9cca7d44d087a0
Karol Kolański <gralu86@gmail.com>
2025-02-16 11:03:31 +0100

    Finished testValidLogin

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,7 +8,9 @@ class HomePageLocators:
     Home Page locators
     """
     LOG_IN_A = (By.ID, "login2")
+    LOG_OUT_A = (By.ID, "logout2")
     NAME_OF_USER_A = (By.ID, "nameofuser")
+    SIGN_UP_A = (By.ID, "signin2")
 
 class HomePage(BasePage):
     """
@@ -25,6 +27,13 @@ class HomePage(BasePage):
         # Zwróć stronę logowania
         return LoginPage(self.driver)
 
+    def click_log_out(self):
+        """
+        Clicks Log out link
+        """
+        self.driver.find_element(*HomePageLocators.LOG_OUT_A).click()
+
+
     def get_welcome_username_text(self):
         """
         Gets Welcome <USERNAME> message from the top right of the page
@@ -33,6 +42,22 @@ class HomePage(BasePage):
         # Czekamy na Welcome <USERNAME>
         self.wait_5s.until(EC.text_to_be_present_in_element(HomePageLocators.NAME_OF_USER_A, "Welcome"))
         return self.driver.find_element(*HomePageLocators.NAME_OF_USER_A).text
+
+    def get_login_text(self):
+        """
+        Gets Login link text
+        :return: Log in text
+        """
+        self.wait_5s.until(EC.text_to_be_present_in_element(HomePageLocators.LOG_IN_A, "Log in"))
+        return self.driver.find_element(*HomePageLocators.LOG_IN_A).text
+
+    def get_sign_up_text(self):
+        """
+        Gets Sign up link text
+        :return: Log in text
+        """
+        self.wait_5s.until(EC.text_to_be_present_in_element(HomePageLocators.SIGN_UP_A, "Sign up"))
+        return self.driver.find_element(*HomePageLocators.SIGN_UP_A).text
 
     def click_contact(self):
         """
--- tests/login_tests.py
+++ tests/login_tests.py
@@ -28,7 +28,10 @@ class LoginTest(BaseTest):
         welcome_text_act = self.home_page.get_welcome_username_text()
         self.assertEqual(f"Welcome {username}", welcome_text_act)
         # 5. (Sprawdź, czy można kliknąć LogOut)
+        self.home_page.click_log_out()
+        # Sprawdź, czy przyciski zmieniły się na Log in, Sign up
+        self.assertEqual("Log in", self.home_page.get_login_text())
+        self.assertEqual("Sign up", self.home_page.get_sign_up_text())
+
         # TODO:
         sleep(1.5)
-
-        https://github.com/kkolanski/Demoblaze2025
