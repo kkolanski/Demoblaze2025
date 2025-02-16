f96fec477461d5be42c59ac7aa6e6ee6812da874
Karol Kolański <gralu86@gmail.com>
2025-02-16 13:09:18 +0100

    run_tests.py added

---
+++ run_tests.py
@@ -0,0 +1,19 @@
+import unittest
+from tests.login_tests import LoginTest
+
+# Ładujemy testy, z TestCase
+login_tests = unittest.TestLoader().loadTestsFromTestCase(LoginTest)
+
+# Lista testów do uruchomienia
+tests_for_run = [
+    login_tests,
+    # ...
+    # ...
+]
+
+# Łączymy testy w Test Suitę
+test_suite = unittest.TestSuite(tests_for_run)
+
+# Odpal testy
+unittest.TextTestRunner().run(test_suite)
+
